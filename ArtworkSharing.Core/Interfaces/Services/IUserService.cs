c16dd667f8885bc43beccec3cfc99600e5504a96
viet-quanggg <nguyenvietquang099@gmail.com>
2024-02-12 13:27:25 +0700

    add service class for Category, User, ArtworkService

---
+++ ArtworkSharing.Core/Interfaces/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ArtworkSharing.Core.Domain.Entities;
+
+namespace ArtworkSharing.Core.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<IList<User>> GetUsers();
+        Task<User> GetUser(Guid userId);
+        Task CreateNewUser(User user);
+        Task UpdateUser(User user);
+        Task DeleteUser(Guid userId);
+    }
+}
+
