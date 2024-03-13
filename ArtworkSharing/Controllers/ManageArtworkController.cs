87522e177d40c6c15a8e05bb2aad5a6ae7f62091
caothang7a7 <caothang7a7@gmail.com>
2024-03-13 14:30:16 +0700

    merge feature

--- ArtworkSharing/Controllers/ManageArtworkController.cs
+++ ArtworkSharing/Controllers/ManageArtworkController.cs
@@ -2,81 +2,141 @@
 using ArtworkSharing.Core.Interfaces.Services;
 using ArtworkSharing.Core.ViewModels.Artworks;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace ArtworkSharing.Controllers
 {
-    [Route("[controller]")]
     [ApiController]
-    public class ManageArtworkController : Controller
+    [Route("[controller]")]
+    public class ManageArtWorkController : ControllerBase
     {
-        private readonly IArtworkService _artworkService;
+        
+        private readonly IArtistService _ArtistService;
+        private readonly IArtworkService _ArtworkService;
+        private readonly ILogger<ManageOrderArtistController> _logger;
 
-        public ManageArtworkController(IArtworkService artworkService)
+        public ManageArtWorkController(IArtistService artistService, IArtworkService artworkService, ILogger<ManageOrderArtistController> logger)
         {
-            _artworkService = artworkService;
+            _ArtistService = artistService;
+            _ArtworkService = artworkService;
+            _logger = logger;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<ArtworkViewModel>>> GetAllArtworks()
-        {
-            var artworks = await _artworkService.GetAll();
-            return Ok(artworks);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ArtworkViewModel>> GetArtwork(Guid id)
-        {
-            var artwork = await _artworkService.GetOne(id);
-            if (artwork == null)
-            {
-                return NotFound();
-            }
-            return Ok(artwork);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateArtwork(Guid id, Artwork artworkInput)
+        [HttpGet("{entityId}", Name = "GetArtworkofArtist")]
+        public async Task<ActionResult<ManageOrderArtistController>> GetCombinedEntityById(Guid entityId, int page)
         {
             try
             {
-                await _artworkService.Update(artworkInput);
-                return Ok(artworkInput);
+                var artists = await _ArtistService.GetOne(entityId);
+                if (artists == null)
+                {
+                    return NotFound("Artist not found");
+                }
+                IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("Page.json", true, true)
+                .Build();
+                var pageSize = int.Parse(configuration.GetSection("Page")["Value"]);
+                var artworks = artists.Artworks
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToList(); ;
+                return Ok(artworks);
             }
-            catch (KeyNotFoundException)
+            catch (Exception ex)
             {
-                return NotFound();
+                _logger.LogError($"Error getting Artwork: {ex.Message}");
+                return StatusCode(500, "Internal server error");
             }
         }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateArtwork(Artwork artworkInput)
+        [HttpPut(Name = "EditArtwork")]
+        public async Task<IActionResult> Update([FromBody] Artwork artworkInput)
         {
+            
             try
             {
-                await _artworkService.Add(artworkInput);
-                return CreatedAtAction(nameof(GetArtwork), new { id = artworkInput.Id }, artworkInput);
-            }
-            catch (Exception)
-            {
-                return StatusCode(500);
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteArtwork(Guid id)
-        {
-            try
-            {
-                await _artworkService.Delete(id);
+                var existArtwork = await _ArtworkService.GetOne(artworkInput.Id);
+                if (existArtwork == null)
+                {
+                    return NotFound("Artwork not found");
+                }
+                existArtwork.Name = artworkInput.Name;
+                existArtwork.Price = artworkInput.Price;
+                existArtwork.Description = artworkInput.Description;
+                await _ArtworkService.Update(existArtwork);
                 return NoContent();
             }
-            catch (KeyNotFoundException)
+            catch (Exception ex)
             {
-                return NotFound();
+                _logger.LogError($"Error updating Artwork: {ex.Message}");
+                return StatusCode(500, "Internal server error");
             }
         }
+
+        [HttpDelete("{artworkId}", Name = "DeleteArtwork")]
+        public async Task<IActionResult> Delete(Guid artworkId)
+        {
+            try
+            {
+                var existingArtwork = await _ArtworkService.GetOne(artworkId);
+                if (existingArtwork == null)
+                {
+                    return NotFound();
+                }
+
+                await _ArtworkService.Delete(artworkId);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error deleting Artwork: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        [HttpPost("{artistId}", Name = "AddlistArtworks")]
+        public async Task<IActionResult> AddArtworks(Guid artistId, [FromBody] List<Artwork> artworks)
+        {
+            try
+            {
+                var artist = await _ArtistService.GetOne(artistId);
+                if (artist == null)
+                {
+                    return NotFound("Artist not found");
+                }
+
+                foreach (var artwork in artworks)
+                {
+                    artwork.Artist = artist;
+                    await _ArtworkService.Add(artwork);
+                }
+
+                return CreatedAtRoute("GetArtworksByArtist", new { artistId = artist.Id }, artworks);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error adding artworks: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+        [HttpPost("{artistId}", Name = "AddArtwork")]
+        public async Task<IActionResult> Add(Guid artistId, [FromBody] Artwork artwork)
+        {
+            try
+            {
+                var artist = await _ArtistService.GetOne(artistId);
+                if (artist == null)
+                {
+                    return NotFound("Artist not found");
+                }
+                artwork.Artist = artist;
+                await _ArtworkService.Add(artwork);
+                return CreatedAtRoute("GetArtworkById", new { artworkId = artwork.ArtistId }, artwork);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error adding Artwork: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
     }
 }
